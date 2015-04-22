3dd7e30da51718211d16f4e528aae2baf67b2cc1
myun2 <myun2@nwhite.info>
2015-04-22 20:44:09 +0900

    Add body (HTML)

--- html.hpp
+++ html.hpp
@@ -1,18 +1,34 @@
 #ifndef __MYUN2_GITHUB_COM__CORNELIUS__HTML_HPP__
 #define __MYUN2_GITHUB_COM__CORNELIUS__HTML_HPP__
 
+#include "html/head.hpp"
+
 namespace myun2
 {
 	namespace cornelius
 	{
 		namespace html
 		{
-			template <unsigned char Version>
-			struct document
+			struct body
 			{
 				::std::string serialize()
 				{
+					::std::string buffer = "<body>";
+					return buffer + "</body>";
+				}
+			};
+
+			template <unsigned char Version>
+			struct document
+			{
+				head _head;
+				body _body;
+
+				::std::string serialize()
+				{
 					::std::string buffer = "<!DOCTYPE html><html>";
+					buffer += _head.serialize();
+					buffer += _body.serialize();
 					return buffer + "</html>";
 				}
 			};
