0fee11fad48b9ddb56d6c2cb56cd15f841a80d80
myun2 <myun2@nwhite.info>
2015-04-22 19:45:00 +0900

    css.hpp, html.hpp, js.hpp add

---
+++ css.hpp
@@ -0,0 +1,14 @@
+#ifndef __MYUN2_GITHUB_COM__CORNELIUS__CSS_HPP__
+#define __MYUN2_GITHUB_COM__CORNELIUS__CSS_HPP__
+
+namespace myun2
+{
+	namespace cornelius
+	{
+		namespace css
+		{
+		}
+	}
+}
+
+#endif//__MYUN2_GITHUB_COM__CORNELIUS__CSS_HPP__
