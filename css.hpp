5091687677c50801691b7ed7197589fb9a03375a
myun2 <myun2@nwhite.info>
2015-04-22 19:50:40 +0900

    Update css.hpp

--- css.hpp
+++ css.hpp
@@ -1,12 +1,20 @@
 #ifndef __MYUN2_GITHUB_COM__CORNELIUS__CSS_HPP__
 #define __MYUN2_GITHUB_COM__CORNELIUS__CSS_HPP__
 
+#include <string>
+#include <map>
+
 namespace myun2
 {
 	namespace cornelius
 	{
 		namespace css
 		{
+			struct selector
+			{
+				::std::string name;
+				::std::map<::std::string, ::std:string> attributes;
+			};
 		}
 	}
 }
