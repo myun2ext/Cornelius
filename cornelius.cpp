a9e32cafb4c1e7062ddcd0182810f47bc3ebdb9d
myun2 <myun2@nwhite.info>
2015-04-24 16:49:05 +0900

    update server

--- cornelius.cpp
+++ cornelius.cpp
@@ -37,8 +37,9 @@ int proccess_request(FILE* in, FILE* out)
 {
 	char buffer[2046];
 	fprintf(stderr, "GET / HTTP/1.1 ?\n");
+	fprintf(stderr, fgets(buffer, sizeof(buffer), in));
 
-	http::request r = http::request::parse(fgets(buffer, sizeof(buffer), in));
+	http::request r = http::request::parse(buffer);
 	fprintf(stderr, "Method:   \"%s\"\n", r.method.c_str());
 	fprintf(stderr, "Path:     \"%s\"\n", r.path.c_str());
 	fprintf(stderr, "Protocol: \"%s\"\n", r.protocol.c_str());
@@ -46,6 +47,7 @@ int proccess_request(FILE* in, FILE* out)
 
 	::std::string content = get_html(r.path.c_str()).serialize().c_str();
 	http::write_request(out, content);
+	http::write_request(stdout, content);
 	printf("\n");
 
 	return 0;
@@ -58,12 +60,13 @@ int server_callback(struct sockaddr_in addr, responder::socket_desc::sock_type s
 	fp = fdopen(_open_osfhandle(sock, _O_RDONLY), "r+b");
 #else
 	fp = fdopen(sock, "r+");
+	setvbuf(fp, NULL, _IONBF, 0);
 #endif
 	return proccess_request(fp, fp);
 }
 
 int server() {
 	fprintf(stderr, "Server listened in 8000 port.\n");
-	responder::listener server(8000, 10, server_callback);
+	responder::listener server(19528, 10, server_callback);
 	return 0;
 }
